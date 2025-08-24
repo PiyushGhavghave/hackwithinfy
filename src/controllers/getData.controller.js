9835e07640527880be8b87d75c2975fa962686ea
PiyushGhavghave <piyushghavghave@gmail.com>
2025-08-24 09:31:19 +0530

    changed get data controller

--- src/controllers/getData.controller.js
+++ src/controllers/getData.controller.js
@@ -1,8 +1,11 @@
 import sosRequestModel from "../models/sosRequest.model.js";
 
 export const getSOSRequests = async (req, res) => {
+    let { requesterId } = req.query;
+    requesterId = "+" + requesterId.trim()
+    console.log("Fetching SOS requests for requesterId:", requesterId);
     try {
-        const requests = await sosRequestModel.find();
+        const requests = await sosRequestModel.find({ requesterId : requesterId });
         res.status(200).json(requests);
     } catch (error) {
         console.error("Error fetching SOS requests:", error);
